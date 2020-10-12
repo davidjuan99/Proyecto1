3bbc7e7e6a814a407fcea6d94c6dff7a9be21caf
David <david.juanaranda@gmail.com>
2020-10-12 17:44:42 +0200

    Arreglados errores

--- index.php
+++ index.php
@@ -1,65 +1,74 @@
-<!DOCTYPE html>
-<html lang="es">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Proyecto 1</title>
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link rel="stylesheet" href="./css/style.css">
-</head>
-<body>
-    <div class="topnav">
-  	<div>
-        <a href="#inicio" class="p_link">Inicio</a>
-  		<a href="#noticias">Noticias</a>
-  		<a href="#galeria">Galeria</a>
-  		<a href="#contacto" class="u_link">Contacto</a>
-     </div>
-    </div>
-    <div class="banner">
+    <link href="https://fonts.googleapis.com/css?family=Anton|Montserrat&display=swap" rel="stylesheet">
+    <title>Tutorial</title>
+  </head>
+  <body>
+   
+    <header class="header">
+        <div class="bg-dark ">
+            <div class="container">
+                <nav class="navbar navbar-expand-lg navbar-light">   
+                    <button class="navbar-toggler custom-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                      <span class=" navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                      <div class=" navg navbar-nav w-100 justify-content-center " >
+                        <a class="nav-item nav-link active" href="#">inicio</a>
+                        <a class="nav-item nav-link" href="#">Galería</a>
+                        <a class="nav-item nav-link" href="#">Noticias</a>
+                        <a class="nav-item nav-link" href="#">contacto</a>
+                      </div>
+                    </div>
+                  </nav>
+            </div>
+        </div>
 
-    </div>
+        <div class="text-header d-flex align-items-center">
+            <div class="container">
+                <div class="row">
+                    <div class="col-sm-8">
+                        <h2> Noticias sobre Tecnología Informática </h2>
+                        <p>Bienvenido a la mejor página web sobre noticias tecnológicas. Dale a "Noticias" para acceder directamente a los últimos post de la web.</p>
+                        <a href="" class="btn btn-yo"> <span>Noticias</span> </a>
+                    </div>
 
-    <p>What is Lorem Ipsum?
-Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+                </div>
+            </div>
+        </div>
 
-Why do we use it?
-It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
+    </header>
 
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc bibendum risus eget sem viverra tincidunt. Mauris maximus purus velit, eu venenatis dui molestie ut. Duis eu libero a risus auctor tincidunt sed in nunc. Mauris molestie mi quam, et tincidunt felis finibus sit amet. Morbi quam eros, malesuada sed dui sollicitudin, consequat gravida mauris. Aenean porta eros ultricies vestibulum vulputate. Suspendisse potenti. Sed nec molestie erat. Maecenas a lectus tempus, bibendum ligula non, dapibus augue. Aenean eget ultrices velit, laoreet rhoncus arcu. Proin nec metus nec felis fermentum congue. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque ut tellus aliquet, ullamcorper odio non, cursus urna. Nulla non leo nisi.
 
-Where does it come from?
-Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.
+Ut sit amet quam ipsum. Maecenas euismod, orci euismod viverra tempus, eros turpis consectetur sapien, a commodo lorem ligula vel ante. Integer congue mauris sed rutrum elementum. Donec odio purus, malesuada eget magna id, mattis tristique lectus. Nam vestibulum elit ipsum, at mollis elit interdum ac. Sed eleifend sodales suscipit. Mauris volutpat libero nulla, sed ullamcorper sem lacinia non. Ut a massa faucibus, mollis ex vitae, dapibus nisi. Curabitur vitae est bibendum sapien aliquam mollis nec vel massa. Nullam aliquet sagittis diam in tincidunt.
 
-The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.
+In hac habitasse platea dictumst. Nulla tortor libero, vehicula egestas risus vel, elementum fringilla arcu. Nullam viverra mollis erat a elementum. Nunc finibus finibus sapien et finibus. Praesent consectetur id eros sed ultrices. Cras sed feugiat metus. Donec sit amet magna at risus scelerisque pulvinar. Vestibulum hendrerit placerat suscipit. Aenean libero purus, mattis eget quam ac, dapibus aliquet nibh. Nam eu augue eget mi feugiat convallis. Donec pulvinar, sem ut aliquet accumsan, ligula ipsum molestie mi, sit amet sagittis dui nulla eu tortor. Etiam a diam id metus consectetur maximus. Nunc in enim blandit, scelerisque nisi id, vulputate magna.
 
-Where can I get some?$a <= 10 Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.
+Praesent dictum dapibus aliquet. Nullam sed orci at nibh ultrices convallis sit amet in sapien. Cras eu ex lorem. Proin elit sapien, tristique nec hendrerit ultrices, pellentesque vel ligula. Curabitur pharetra laoreet tortor nec interdum. Praesent at congue mauris. Etiam congue risus at sodales molestie.
 
-5
-	paragraphs
-	words
-	bytes
-	lists
-	Start with 'Lorem
-ipsum dolor sit amet...'
+Sed tincidunt imperdiet mauris, a eleifend enim convallis et. Ut feugiat efficitur tellus ut tempor. Maecenas sollicitudin dui gravida pellentesque egestas. Duis sed viverra ante. Interdum et malesuada fames ac ante ipsum primis in faucibus. Pellentesque ornare ultricies arcu vitae consectetur. Cras at est lobortis, sollicitudin lacus a, malesuada lorem. Sed tristique eros turpis, sed convallis erat convallis a. Morbi luctus nibh quam. Proin blandit tincidunt massa, vel rhoncus neque placerat id. Cras lobortis dui in commodo tincidunt. Sed porta mi eget metus placerat, non elementum ligula vestibulum. Ut sodales eleifend diam, sit amet scelerisque lacus. Maecenas gravida fringilla felis, ut ornare ex scelerisque sed. Aliquam malesuada purus pulvinar augue sollicitudin euismod. Fusce semper sapien nisl, sit amet dignissim velit condimentum sed.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc bibendum risus eget sem viverra tincidunt. Mauris maximus purus velit, eu venenatis dui molestie ut. Duis eu libero a risus auctor tincidunt sed in nunc. Mauris molestie mi quam, et tincidunt felis finibus sit amet. Morbi quam eros, malesuada sed dui sollicitudin, consequat gravida mauris. Aenean porta eros ultricies vestibulum vulputate. Suspendisse potenti. Sed nec molestie erat. Maecenas a lectus tempus, bibendum ligula non, dapibus augue. Aenean eget ultrices velit, laoreet rhoncus arcu. Proin nec metus nec felis fermentum congue. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque ut tellus aliquet, ullamcorper odio non, cursus urna. Nulla non leo nisi.
 
-Translations: Can you help translate this site into a foreign language ? Please email us with details if you can help.
-There are now a set of mock banners available here in three colours and in a range of standard banner sizes:
-BannersBannersBanners
-Donate: If you use this site regularly and would like to help keep the site on the Internet, please consider donating a small sum to help pay for the hosting and bandwidth bill. There is no minimum donation, any sum is appreciated - click here to donate using PayPal. Thank you for your support.
-Firefox Add-on NodeJS TeX Package Python Interface GTK Lipsum Rails .NET Groovy Adobe Plugin
-The standard Lorem Ipsum passage, used since the 1500s
-"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+Ut sit amet quam ipsum. Maecenas euismod, orci euismod viverra tempus, eros turpis consectetur sapien, a commodo lorem ligula vel ante. Integer congue mauris sed rutrum elementum. Donec odio purus, malesuada eget magna id, mattis tristique lectus. Nam vestibulum elit ipsum, at mollis elit interdum ac. Sed eleifend sodales suscipit. Mauris volutpat libero nulla, sed ullamcorper sem lacinia non. Ut a massa faucibus, mollis ex vitae, dapibus nisi. Curabitur vitae est bibendum sapien aliquam mollis nec vel massa. Nullam aliquet sagittis diam in tincidunt.
 
-Section 1.10.32 of "de Finibus Bonorum et Malorum", written by Cicero in 45 BC
-"Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"
+In hac habitasse platea dictumst. Nulla tortor libero, vehicula egestas risus vel, elementum fringilla arcu. Nullam viverra mollis erat a elementum. Nunc finibus finibus sapien et finibus. Praesent consectetur id eros sed ultrices. Cras sed feugiat metus. Donec sit amet magna at risus scelerisque pulvinar. Vestibulum hendrerit placerat suscipit. Aenean libero purus, mattis eget quam ac, dapibus aliquet nibh. Nam eu augue eget mi feugiat convallis. Donec pulvinar, sem ut aliquet accumsan, ligula ipsum molestie mi, sit amet sagittis dui nulla eu tortor. Etiam a diam id metus consectetur maximus. Nunc in enim blandit, scelerisque nisi id, vulputate magna.
 
-1914 translation by H. Rackham
-"But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness. No one rejects, dislikes, or avoids pleasure itself, because it is pleasure, but because those who do not know how to pursue pleasure rationally encounter consequences that are extremely painful. Nor again is there anyone who loves or pursues or desires to obtain pain of itself, because it is pain, but because occasionally circumstances occur in which toil and pain can procure him some great pleasure. To take a trivial example, which of us ever undertakes laborious physical exercise, except to obtain some advantage from it? But who has any right to find fault with a man who chooses to enjoy a pleasure that has no annoying consequences, or one who avoids a pain that produces no resultant pleasure?"
+Praesent dictum dapibus aliquet. Nullam sed orci at nibh ultrices convallis sit amet in sapien. Cras eu ex lorem. Proin elit sapien, tristique nec hendrerit ultrices, pellentesque vel ligula. Curabitur pharetra laoreet tortor nec interdum. Praesent at congue mauris. Etiam congue risus at sodales molestie.
 
-Section 1.10.33 of "de Finibus Bonorum et Malorum", written by Cicero in 45 BC
-"At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat."
+Sed tincidunt imperdiet mauris, a eleifend enim convallis et. Ut feugiat efficitur tellus ut tempor. Maecenas sollicitudin dui gravida pellentesque egestas. Duis sed viverra ante. Interdum et malesuada fames ac ante ipsum primis in faucibus. Pellentesque ornare ultricies arcu vitae consectetur. Cras at est lobortis, sollicitudin lacus a, malesuada lorem. Sed tristique eros turpis, sed convallis erat convallis a. Morbi luctus nibh quam. Proin blandit tincidunt massa, vel rhoncus neque placerat id. Cras lobortis dui in commodo tincidunt. Sed porta mi eget metus placerat, non elementum ligula vestibulum. Ut sodales eleifend diam, sit amet scelerisque lacus. Maecenas gravida fringilla felis, ut ornare ex scelerisque sed. Aliquam malesuada purus pulvinar augue sollicitudin euismod. Fusce semper sapien nisl, sit amet dignissim velit condimentum sed.</p>
 
-1914 translation by H. Rackham
-"On the other hand, we denounce with righteous indignation and dislike men who are so beguiled and demoralized by the charms of pleasure of the moment, so blinded by desire, that they cannot foresee the pain and trouble that are bound to ensue; and equal blame belongs to those who fail in their duty through weakness of will, which is the same as saying through shrinking from toil and pain. These cases are perfectly simple and easy to distinguish. In a free hour, when our power of choice is untrammelled and when nothing prevents our being able to do what we like best, every pleasure is to be welcomed and every pain avoided. But in certain circumstances and owing to the claims of duty or the obligations of business it will frequently occur that pleasures have to be repudiated and annoyances accepted. The wise man therefore always holds in these matters to this principle of selection: he rejects pleasures to secure other greater pleasures, or else he endures pains to avoid worse pains."</p>
-
-</body>
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  </body>
 </html>
